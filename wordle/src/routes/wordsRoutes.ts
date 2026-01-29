2dd8e5e018ca0228d630865626eed06c8ce52f79
amit reuveni <amitr123r@gmail.com>
2026-01-29 13:20:05 +0200

    organized the files

--- wordle/src/routes/wordsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as wordsControlles from "../controllers/wordsControllers";
-
-const router = express.Router();
-
-router.get("/get-random-word", wordsControlles.getRandomWord);
-router.post("/check-if-exist", wordsControlles.checkIfExist);
-
-export default router;
